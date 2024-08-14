487c0e48ae6c0167c946139ce1cda08b93bdd5ae
Ramiro Medina <64783088+ramedina86@users.noreply.github.com>
2024-08-14 13:51:01 +0000

    Revert "chore(internal): codegen related update (#57)"
    This reverts commit a1541509487aff4a5434366f1629d4729d13b692.

--- src/writerai/resources/files.py
+++ src/writerai/resources/files.py
@@ -211,6 +211,7 @@ class FilesResource(SyncAPIResource):
         *,
         content: FileTypes,
         content_disposition: str,
+        content_type: str,
         # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
         # The extra values given here take precedence over values defined on the client or passed to this method.
         extra_headers: Headers | None = None,
@@ -230,7 +231,11 @@ class FilesResource(SyncAPIResource):
 
           timeout: Override the client-level default timeout for this request, in seconds
         """
-        extra_headers = {"Content-Disposition": content_disposition, **(extra_headers or {})}
+        extra_headers = {
+            "Content-Disposition": content_disposition,
+            "Content-Type": content_type,
+            **(extra_headers or {}),
+        }
         return self._post(
             "/v1/files",
             options=make_request_options(
@@ -419,6 +424,7 @@ class AsyncFilesResource(AsyncAPIResource):
         *,
         content: FileTypes,
         content_disposition: str,
+        content_type: str,
         # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
         # The extra values given here take precedence over values defined on the client or passed to this method.
         extra_headers: Headers | None = None,
@@ -438,7 +444,11 @@ class AsyncFilesResource(AsyncAPIResource):
 
           timeout: Override the client-level default timeout for this request, in seconds
         """
-        extra_headers = {"Content-Disposition": content_disposition, **(extra_headers or {})}
+        extra_headers = {
+            "Content-Disposition": content_disposition,
+            "Content-Type": content_type,
+            **(extra_headers or {}),
+        }
         return await self._post(
             "/v1/files",
             options=make_request_options(
