9ce59b1bbc7a07459c01cbc38e8a124d165b7160
David Meadows <dtmeadows@stainlessapi.com>
2024-09-24 13:48:28 -0400

    fix(lint): format

--- src/writerai/resources/files.py
+++ src/writerai/resources/files.py
@@ -9,7 +9,7 @@ import httpx
 
 from ..types import file_list_params, file_retry_params
 from .._types import NOT_GIVEN, Body, Query, Headers, NotGiven, FileTypes
-from .._utils import maybe_transform
+from .._utils import maybe_transform, async_maybe_transform
 from .._compat import cached_property
 from .._resource import SyncAPIResource, AsyncAPIResource
 from .._response import (
--- src/writerai/types/chat_streaming_data.py
+++ src/writerai/types/chat_streaming_data.py
@@ -1,7 +1,6 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
 
-
 from .chat import Chat
 from .._models import BaseModel
 
