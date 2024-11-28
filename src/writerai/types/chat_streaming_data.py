c33a2932a49d4d51ad81045ba312162f02821cbc
Samuel El-Borai <sam@elborai.me>
2024-11-28 21:39:04 +0100

    chore: fix formatting

--- src/writerai/types/chat_streaming_data.py
+++ src/writerai/types/chat_streaming_data.py
@@ -1,8 +1,8 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
 
-from .._models import BaseModel
 from .chat import Chat
+from .._models import BaseModel
 
 __all__ = ["ChatStreamingData"]
 
